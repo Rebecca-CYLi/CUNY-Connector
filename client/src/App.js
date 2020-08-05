4516ad111b2c11dfeee00d595ab91868494fb109
Rebecca Li <Rebecca.CYLi@gmail.com>
2020-08-04 21:54:31 -0400

    new updates

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SababaForm from './Form.js';
+
+function App() {
+  return (
+    <div className="App">
+        <SababaForm />
+    </div>
+  );
+}
+
+
+export default App;
